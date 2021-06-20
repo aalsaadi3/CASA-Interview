11b4af21e5cea29fff2881e98a6778d3cd020895
Ahmad Al Saadi <48738728+aalsaadi3@users.noreply.github.com>
2021-06-19 20:11:07 -0500

    First Attempt
    We attempt using a divisibility counter and iterating from i=1 to i=n; for every i value n is divisible by we increment the counter variable; if the counter variable equals 2 we return true.

--- Program.cs
+++ Program.cs
@@ -6,17 +6,27 @@ namespace CASA_Interview
     {
         static void Main(string[] args)
         {
-            // IsPrime(5);
-            Console.WriteLine(5 % 1);
-            Console.WriteLine(5 % 2);
+            IsPrime(5);
             //maybe have a counter to keep track of how many i values
             //yield result of n%i = 0
 
 
         }
-        // public Boolean IsPrime(int n)
-        // {
-
-        // }
+        public Boolean IsPrime(int n)
+        {
+            //divisibility counter
+            int counter = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    counter += 1;
+                }
+            }
+            if (counter == 2)
+            {
+                return true;
+            }
+        }
     }
 }
