542cb7272688d4a9882dbcd0b5a797eacc824f22
Ahmad Al Saadi <48738728+aalsaadi3@users.noreply.github.com>
2021-06-19 19:44:17 -0500

    Getting Started
    Changed "Hello World!" output to something else.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CASA_Interview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Getting started on Requested Problem");
+        }
+    }
+}
